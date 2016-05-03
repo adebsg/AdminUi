2be076142ba61462f6c6e77394a1fa2010a27552
adebsg <adebsg@gmail.com>
2016-05-03 16:14:21 +0700

    4

---
+++ backend/views/cat_here/index.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\Cat_hereSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Cat Heres');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="cat-here-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Cat Here'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'title',
+            'icon:ntext',
+            'type',
+            'href:ntext',
+            // 'system',
+            // 'within:ntext',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
