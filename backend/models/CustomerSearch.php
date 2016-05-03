b220a10c23389e854214166ba73e6cae14a327f1
adebsg <adebsg@gmail.com>
2016-05-03 16:06:25 +0700

    4

---
+++ backend/models/CustomerSearch.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use backend\models\Customer;
+
+/**
+ * CustomerSearch represents the model behind the search form about `backend\models\Customer`.
+ */
+class CustomerSearch extends Customer
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'id_profile'], 'integer'],
+            [['username', 'password', 'email', 'facebook_id'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Customer::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'id_profile' => $this->id_profile,
+        ]);
+
+        $query->andFilterWhere(['like', 'username', $this->username])
+            ->andFilterWhere(['like', 'password', $this->password])
+            ->andFilterWhere(['like', 'email', $this->email])
+            ->andFilterWhere(['like', 'facebook_id', $this->facebook_id]);
+
+        return $dataProvider;
+    }
+}
