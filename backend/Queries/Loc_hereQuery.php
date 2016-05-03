5c14b008c784c1fdf2c50159c9fb31f7757f973a
adebsg <adebsg@gmail.com>
2016-05-03 16:12:14 +0700

    4

---
+++ backend/Queries/Loc_hereQuery.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace backend\Queries;
+
+/**
+ * This is the ActiveQuery class for [[\backend\models\Loc_here]].
+ *
+ * @see \backend\models\Loc_here
+ */
+class Loc_hereQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        $this->andWhere('[[status]]=1');
+        return $this;
+    }*/
+
+    /**
+     * @inheritdoc
+     * @return \backend\models\Loc_here[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * @inheritdoc
+     * @return \backend\models\Loc_here|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
